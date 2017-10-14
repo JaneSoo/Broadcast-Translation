0aaa11edada791cba9f627a5a138c23870856c92
Kanitha <chimkanitha@gmail.com>
2017-10-14 10:23:54 +0700

    (feature):event event crud

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :events
   get 'dashboard/index'
   root 'dashboard#index'
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171014025329) do
+ActiveRecord::Schema.define(version: 20171014031053) do
+
+  create_table "events", force: :cascade do |t|
+    t.string "title"
+    t.string "venue"
+    t.datetime "start_date"
+    t.datetime "end_date"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_events_on_user_id"
+  end
+
+  create_table "titles", force: :cascade do |t|
+    t.string "venue"
+    t.datetime "start_date"
+    t.datetime "end_date"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_titles_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
